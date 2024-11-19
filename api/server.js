1709cea16aa5ba7c9e52ca9bb1fa6fc19c93d6ee
Ibrahim Hakam <77873485+ibrahimtheboss@users.noreply.github.com>
2024-11-19 09:08:38 +0400

    new update 3

--- api/server.js
+++ api/server.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const path = require('path');
 require('dotenv').config();
 
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -13,7 +14,9 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+console.log('MongoDB URI:', process.env.MONGO_URI);
+
+mongoose.connect('mongodb+srv://test:test@cluster0.2hzbiju.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.error(err));
 
