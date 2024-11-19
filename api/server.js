991a58ebc0c78f38dd3c958cb61280fe58eb8f98
Ibrahim Hakam <77873485+ibrahimtheboss@users.noreply.github.com>
2024-11-19 08:12:22 +0400

    new fix

--- api/server.js
+++ api/server.js
@@ -10,7 +10,6 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'client/build')));
 
 // MongoDB Connection
 mongoose
@@ -23,10 +22,12 @@ app.use('/api/students', require('./routes/studentRoutes'));
 app.use('/api/subjects', require('./routes/subjectRoutes'));
 
 // React App Route
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+  });
+}
 
-// Start the server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// Export as a Serverless Function
+module.exports = app;
