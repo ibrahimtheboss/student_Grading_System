8d219a49394c51cba0e72e52aad585563a874d8d
Ibrahim Hakam <77873485+ibrahimtheboss@users.noreply.github.com>
2024-11-19 08:27:32 +0400

    small update1
    small update1

--- api/server.js
+++ api/server.js
@@ -23,10 +23,13 @@ app.use('/api/subjects', require('./routes/subjectRoutes'));
 
 // React App Route
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-  });
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+// The "catchall" handler: for any request that doesn't match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/../client/build/index.html'));
+});
 }
 
 // Export as a Serverless Function
--- client/src/api.js
+++ client/src/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 /*const API = axios.create({ baseURL: 'http://localhost:5000/api' });*/
-const API = axios.create({ baseURL: '/api' });
+const API = axios.create({ baseURL: process.env.REACT_APP_API_URL || '/api' });
 export default API;
